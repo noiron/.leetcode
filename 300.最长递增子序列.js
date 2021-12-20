be56501ae74ab4554f796a2f34ef90d2a2cb98c5
wu kai <wukai2048@gmail.com>
2021-12-20 21:35:18 +0800

    lc300

---
+++ 300.最长递增子序列.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  const dp = new Array(nums.length).fill(1);
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1); 
+      }
+    }
+    result = Math.max(result, dp[i]);
+  }
+
+  return result;
+};
+// @lc code=end
+
