fbda4f19e1a61b065b362f1b7a2195103b16283d
wu kai <wukai2048@gmail.com>
2021-12-28 14:20:46 +0800

    lc78

--- 78.子集.js
+++ 78.子集.js
@@ -11,17 +11,22 @@
  */
 var subsets = function (nums) {
   const ans = [];
+  const n = nums.length;
 
-  for (let mask = 0; mask < (1 << nums.length); mask++) {
+  // mask 中为1的位置需要保留，依次检查所有可能的 mask，0 ~ 2^n-1
+  for (let mask = 0; mask < 1 << n; mask++) {
     const currentSet = [];
-    for (let i = 0; i < nums.length; i++) {
-      if (mask & 1 << i) {
+    for (let i = 0; i < n; i++) {
+      // 1 << i 只有一位是1，即当前需要检查的位置
+      if (mask & (1 << i)) {
         currentSet.push(nums[i]);
       }
     }
+
     ans.push(currentSet);
   }
-
   return ans;
 };
 // @lc code=end
+
+subsets([1, 2, 3]);
