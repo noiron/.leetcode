3c9ca9c2132130ab20431ff4a41989709097145f
wu kai <wukai2048@gmail.com>
2022-01-12 17:56:03 +0800

    lc86

---
+++ 86.分隔链表.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=javascript
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+  let small = new ListNode(0);
+  let large = new ListNode(0);
+  const smallHead = small;
+  const largeHead = large;
+
+  while (head) {
+    if (head.val < x) {
+      small.next = head;
+      small = small.next;
+    } else {
+      large.next = head;
+      large = large.next;
+    }
+    head = head.next;
+  }
+  small.next = largeHead.next;
+  large.next = null;
+  return smallHead.next;
+};
+// @lc code=end
