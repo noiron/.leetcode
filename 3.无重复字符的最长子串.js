c31fd78079bbfec7623b1df213b480ecda184124
wu kai <wukai2048@gmail.com>
2022-01-25 11:22:08 +0800

    lc3 update

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -11,16 +11,18 @@
  */
 var lengthOfLongestSubstring = function (s) {
   let result = 0;
-  let left = 0;
-  let right = 0;
+  let left = 0; // 当前字符串的第一个字符的位置
+  let right = 0; // 当前字符串之后的第一个位置
   while (left <= right && right < s.length) {
+    // 如果 left = right，则 currentStr 为空
     const currentStr = s.slice(left, right);
     const currentChar = s[right];
     const index = currentStr.indexOf(currentChar);
     if (index > -1) {
+      // 缩短左侧边界，注意这里是在原来的 left 基础上加上 (index + 1)
       left += index + 1;
     } else {
-      result = Math.max(right - left + 1, result);
+      result = Math.max(currentStr.length + 1, result);
       right++;
     }
   }
@@ -30,3 +32,4 @@ var lengthOfLongestSubstring = function (s) {
 // @lc code=end
 
 console.log(lengthOfLongestSubstring('abcabcbb'));
+// console.log(lengthOfLongestSubstring('bbbbbbb'));
