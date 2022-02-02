0540b50b4954544000a26f010e907e8ec7631d4c
wu kai <wukai2048@gmail.com>
2022-02-02 22:46:28 +0800

    lc334 贪心

--- 334.递增的三元子序列.js
+++ 334.递增的三元子序列.js
@@ -4,6 +4,8 @@
  * [334] 递增的三元子序列
  */
 
+// https://leetcode-cn.com/problems/increasing-triplet-subsequence/solution/c-xian-xing-shi-jian-fu-za-du-xiang-xi-jie-xi-da-b/
+
 // @lc code=start
 /**
  * @param {number[]} nums
@@ -13,23 +15,17 @@ var increasingTriplet = function (nums) {
   const len = nums.length;
   if (len < 3) return false;
 
-  // leftMin 中保存位置 i 及其左侧的最小值
-  const leftMin = new Array(len).fill(0);
-  leftMin[0] = nums[0];
+  let small = nums[0];
+  let mid = Infinity;
   for (let i = 1; i < len; i++) {
-    leftMin[i] = Math.min(leftMin[i - 1], nums[i]);
-  }
-
-  // rightMax 中保存位置 i 及其右侧的最大值
-  const rightMax = new Array(len).fill(0);
-  rightMax[len - 1] = nums[len - 1];
-  for (let i = len - 2; i >= 0; i--) {
-    rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
-  }
-
-  for (let i = 1; i < len - 1; i++) {
-    if (nums[i] > leftMin[i - 1] && nums[i] < rightMax[i + 1]) {
-      return true;
+    const num = nums[i];
+    if (num > mid) return true;
+    if (num <= small) {
+      small = num;
+      continue;
+    }
+    if (num <= mid) {
+      mid = num;
     }
   }
   return false;
