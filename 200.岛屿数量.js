a1a5dad8838e32aa37f0c3f00f81a90f62fb86a9
wu kai <wukai2048@gmail.com>
2022-02-04 22:50:20 +0800

    lc200

--- 200.岛屿数量.js
+++ 200.岛屿数量.js
@@ -13,27 +13,34 @@ var numIslands = function (grid) {
   const rows = grid.length;
   const cols = grid[0].length;
 
-  function dfs(r, c) {
-    if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === '0') return;
+  function dfs(row, col) {
+    if (
+      row < 0 ||
+      col < 0 ||
+      row >= rows ||
+      col >= cols ||
+      grid[row][col] === '0'
+    ) {
+      return;
+    }
 
-    grid[r][c] = '0';
-    dfs(r - 1, c);
-    dfs(r + 1, c);
-    dfs(r, c-1);
-    dfs(r, c+1);
+    grid[row][col] = '0';
+    dfs(row - 1, col);
+    dfs(row + 1, col);
+    dfs(row, col - 1);
+    dfs(row, col + 1);
   }
 
-  let ans = 0;
-
+  let result = 0;
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       if (grid[i][j] === '1') {
-        ans++;
+        result++;
         dfs(i, j);
       }
     }
   }
 
-  return ans;
+  return result;
 };
 // @lc code=end
