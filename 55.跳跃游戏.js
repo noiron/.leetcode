537c0c6397eecfeef43dc5a650374a570f4148cc
wu kai <wukai2048@gmail.com>
2022-02-08 21:50:14 +0800

    update lc55

--- 55.跳跃游戏.js
+++ 55.跳跃游戏.js
@@ -12,15 +12,19 @@
 var canJump = function (nums) {
   const len = nums.length;
   if (len === 1) return true;
+  // 当前能达到的最远位置
   let max = 0;
 
-  for (let i = 0; i < len - 1; i++) {
-    const dis = nums[i];
-    if (i <= max && dis) {
-      for (let j = 1; j <= dis; j++) {
-        max = Math.max(max, i + j);
-        if (max >= len - 1) return true;
-      }
+  for (let i = 0; i < len; i++) {
+    const currentDistance = nums[i];
+
+    // i > max 表示当前的位置i是不可达到的
+    if (i > max) return false;
+    
+    if (currentDistance === 0) continue;
+    for (let j = 1; j <= currentDistance; j++) {
+      max = Math.max(max, i + j);
+      if (max >= len - 1) return true;
     }
   }
   return false;
@@ -29,3 +33,4 @@ var canJump = function (nums) {
 
 console.log(canJump([2, 0]) === true);
 console.log(canJump([2, 3, 1, 1, 4]) === true);
+console.log(canJump([3, 2, 1, 0, 4]) === false);
