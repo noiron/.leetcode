7496536b4a4f18970f607044bccb8b44dbfabceb
wu kai <wukai2048@gmail.com>
2022-02-09 11:27:36 +0800

    lc46

--- 46.全排列.js
+++ 46.全排列.js
@@ -13,25 +13,31 @@ var permute = function (nums) {
   const result = [];
   const path = [];
   const len = nums.length;
+  // used 是一个布尔值的数组，每一位代表 nums 中的对应位置的数字是否已使用
+  const used = new Array(len).fill(false);
 
-  function backtrack(used) {
+  function backtrack() {
     if (path.length === len) {
       result.push([...path]);
       return;
     }
 
     for (let i = 0; i < len; i++) {
+      // 如果这个数字已使用，则检查下一个数字
       if (used[i]) continue;
+
       path.push(nums[i]);
       used[i] = true;
-      backtrack(used);
+      backtrack();
       path.pop();
       used[i] = false;
     }
   }
 
-  backtrack([]);
+  backtrack();
   return result;
 };
 
 // @lc code=end
+
+permute([1, 2, 3]);
