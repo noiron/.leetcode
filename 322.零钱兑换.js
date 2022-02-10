a9ebd452bb50d4d2f29700974ddf5f5d6f6f06ed
wu kai <wukai2048@gmail.com>
2022-02-10 14:04:25 +0800

    update lc322

--- 322.零钱兑换.js
+++ 322.零钱兑换.js
@@ -13,29 +13,29 @@
 var coinChange = function (coins, amount) {
   const memo = new Array(amount + 1).fill(0);
 
-  function change(left) {
-    if (left < 0) {
-      return -1;
-    }
-    if (left === 0) {
-      return 0;
-    }
-    if (memo[left] !== 0) return memo[left];
+  function change(amount) {
+    if (amount < 0) return -1;
+    if (amount === 0) return 0;
+
+    if (memo[amount]) return memo[amount];
+
     let min = Infinity;
     for (const coin of coins) {
-      const count = change(left - coin);
-      if (count >= 0 && count < min) {
+      const count = change(amount - coin);
+      // -1 代表无解，那么继续检查下一个硬币
+      if (count === -1) continue;
+      if (count + 1 < min) {
         min = count + 1;
       }
     }
-    memo[left] = (min === Infinity) ? -1 : min;
-    return memo[left];
+    memo[amount] = min === Infinity ? -1 : min;
+    return memo[amount];
   }
 
-  change(amount);
-  return memo[amount];
+  return change(amount);
 };
 // @lc code=end
 
 console.log(coinChange([1, 2, 5], 100));
 console.log(coinChange([2], 3));
+console.log(coinChange([2, 5, 10, 1], 27));
