e868eb50100311ef29d1027e8f690dc12dc593fb
wu kai <wukai2048@gmail.com>
2022-02-11 14:48:48 +0800

    lc300 二分法

--- 300.最长递增子序列.js
+++ 300.最长递增子序列.js
@@ -10,22 +10,29 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
-  const len = nums.length;
-  // dp[i] 代表的是以 nums[i] 为结尾的子字符串的最大长度
-  const dp = new Array(len).fill(1);
-  let result = 0;
+  // tails[i] 表示长度为 i + 1 的上升子序列结尾元素的可能最小值
+  // tails 是一个严格上升数组
+  const tails = [];
 
-  for (let i = 0; i < len; i++) {
-    // 依次检查 nums[i] 之前的每一个数，是否有比 nums[i] 小的数
-    // 如果存在，则将 nums[i] 加入到原来的子串的最后
-    for (let j = 0; j < i; j++) {
-      if (nums[j] < nums[i]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-      }
+  for (const num of nums) {
+    // 在 tails 中找到第一个大于 num 的数（假设为第 k + 1 个数）
+    // 前面的k个数 < num < tails[k] < tails[k] 后面的数
+    // 将 tails[k] 替换为 num 后，tails 仍然保持严格上升，已经找到的子序列长度保持不变
+    let left = 0;
+    let right = tails.length;
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (tails[mid] < num) left = mid + 1;
+      else right = mid;
     }
-    result = Math.max(result, dp[i]);
+    tails[left] = num;
+    if (tails.length === right) {
+      tails.push(num);
+    }
   }
 
-  return result;
+  return tails.length;
 };
 // @lc code=end
+
+console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]));
