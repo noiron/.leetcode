88fb03b82f3b43bab8ca50e965e4f31ded4339ea
wu kai <wukai2048@gmail.com>
2022-02-11 11:11:19 +0800

    lc300

--- 300.最长递增子序列.js
+++ 300.最长递增子序列.js
@@ -9,14 +9,18 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function(nums) {
-  const dp = new Array(nums.length).fill(1);
+var lengthOfLIS = function (nums) {
+  const len = nums.length;
+  // dp[i] 代表的是以 nums[i] 为结尾的子字符串的最大长度
+  const dp = new Array(len).fill(1);
   let result = 0;
 
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < len; i++) {
+    // 依次检查 nums[i] 之前的每一个数，是否有比 nums[i] 小的数
+    // 如果存在，则将 nums[i] 加入到原来的子串的最后
     for (let j = 0; j < i; j++) {
       if (nums[j] < nums[i]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1); 
+        dp[i] = Math.max(dp[i], dp[j] + 1);
       }
     }
     result = Math.max(result, dp[i]);
@@ -25,4 +29,3 @@ var lengthOfLIS = function(nums) {
   return result;
 };
 // @lc code=end
-
