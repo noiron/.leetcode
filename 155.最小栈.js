ed419ee98adb5228c35897ebcba7884bddb97f57
wu kai <wukai2048@gmail.com>
2022-04-04 17:05:00 +0800

    update lc155

--- 155.最小栈.js
+++ 155.最小栈.js
@@ -17,9 +17,7 @@ var MinStack = function () {
  */
 MinStack.prototype.push = function (val) {
   this.stack.push(val);
-  this.minStack.push(
-    Math.min(this.minStack[this.minStack.length - 1], val)
-  );
+  this.minStack.push(Math.min(this.minStack.at(-1), val));
 };
 
 /**
@@ -27,21 +25,21 @@ MinStack.prototype.push = function (val) {
  */
 MinStack.prototype.pop = function () {
   this.minStack.pop();
-  return this.stack.pop();
+  this.stack.pop();
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function () {
-  return this.stack[this.stack.length - 1];
+  return this.stack.at(-1);
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function () {
-  return this.minStack[this.minStack.length - 1];
+  return this.minStack.at(-1);
 };
 
 /**
