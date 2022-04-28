683fea4b3ed7301f59c4f1c108bb896fa9b551cf
wu kai <wukai2048@gmail.com>
2022-04-28 11:31:55 +0800

    lc128 update

--- 128.最长连续序列.js
+++ 128.最长连续序列.js
@@ -12,22 +12,23 @@
 var longestConsecutive = function (nums) {
   const numSet = new Set(nums);
   let longestStreak = 0;
-
+  
   for (const num of numSet) {
-    if (!numSet.has(num - 1)) {
-      let currentStreak = 1;
-      let currentNum = num;
+    // 如果遇到了 num，就依次检查 num+1, num+2... 是否存在
+    // 如果 num-1 存在，说明遇到 num-1 的时候（可能在之前也可能在之后）也会检查 num，这里可以跳过
+    if (numSet.has(num - 1)) continue;
 
-      while (numSet.has(currentNum + 1)) {
-        currentStreak++;
-        currentNum++;
-      }
-      longestStreak = Math.max(longestStreak, currentStreak);
+    let currentStreak = 1;
+    let currentNum = num + 1;
+
+    while (numSet.has(currentNum)) {
+      currentStreak++;
+      currentNum++;
     }
+    longestStreak = Math.max(longestStreak, currentStreak);
   }
-
   return longestStreak;
 };
 // @lc code=end
 
-// longestConsecutive([1, 2, 3]);
+longestConsecutive([0,3,7,2,5,8,4,6,0,1]);
