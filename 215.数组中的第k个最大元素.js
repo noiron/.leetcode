937a843e16975e35051d73975a7c3537d550700a
wu kai <wukai2048@gmail.com>
2022-06-20 14:20:29 +0800

    update

--- 215.数组中的第k个最大元素.js
+++ 215.数组中的第k个最大元素.js
@@ -11,7 +11,63 @@
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
-  nums.sort((a, b) => b - a);
-  return nums[k - 1];
+  const minHeap = new MinHeap(k);
+  nums.forEach(num => minHeap.add(num))
+  return minHeap.remove();
 };
+
+class MinHeap {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.value = [];
+  }
+
+  add(val) {
+    this.value.push(val);
+    this.bubbleUp(this.value.length - 1);
+    if (this.value.length > this.capacity) {
+      this.remove();
+    }
+  }
+
+  remove() {
+    this.swap(0, this.value.length - 1);
+    const min = this.value.pop();
+    this.bubbleDown(0);
+    return min;
+  }
+
+  bubbleUp(index) {
+    const parent = Math.floor((index - 1) / 2);
+    let max = index;
+    if (parent >= 0 && this.value[parent] > this.value[index]) {
+      max = parent;
+    }
+    if (max !== index) {
+      this.swap(index, max);
+      this.bubbleUp(max);
+    }
+  }
+
+  bubbleDown(index) {
+    const left = index * 2 + 1;
+    const right = index * 2 + 2;
+    let min = index;
+
+    if (left < this.value.length && this.value[left] < this.value[index]) {
+      min = left;
+    }
+    if (right < this.value.length && this.value[right] < this.value[min]) {
+      min = right;
+    }
+    if (min !== index) {
+      this.swap(index, min);
+      this.bubbleDown(min);
+    }
+  }
+
+  swap(i, j) {
+    [this.value[i], this.value[j]] = [this.value[j], this.value[i]];
+  }
+}
 // @lc code=end
---
+++ 450.删除二叉搜索树中的节点.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=javascript
+ *
+ * [450] 删除二叉搜索树中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function (root, key) {
+  if (!root) return null;
+  if (key < root.val) root.left = deleteNode(root.left, key);
+  else if (key > root.val) root.right = deleteNode(root.right, key);
+  else {
+    if (!root.left) return root.right;
+    if (!root.right) return root.left;
+    // 找到右子树中最小的节点，将 root.left 设为其左节点
+    let node = root.right;
+    while (node.left) {
+      node = node.left;
+    }
+    node.left = root.left;
+    root = root.right;
+  }
+  return root;
+};
+// @lc code=end
